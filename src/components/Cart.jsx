a96dddebd2988c8cbdd71df95d0e5f61793cd455
santa <santhosh.003z@gmail.com>
2025-01-23 23:13:15 +0530

    feat: added product cards

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 export default function Cart() {
+  const [cart, setCart] = useState([]);
+
+  function addCart(data) {
+    let tempData = [...cart];
+    tempData.push(data);
+    setCart(tempData);
+  }
+
+  function removeCart(data) {
+    let tempData = cart.filter((item) => item.id !== data.id);
+    setCart(tempData);
+  }
+
   return (
     <>
-      <h2>Cart</h2>
+      <h2>{cart}</h2>
     </>
   );
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import Cards from "./Cards";
+
 export default function Shop() {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  function addToCart(item) {
+    console.log(item);
+  }
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      try {
+        const res = await fetch(`https://fakestoreapi.com/products`);
+        if (!res.ok) throw new Error(`HTTP error: Status ${res.status}`);
+        const data = await res.json();
+        setData(data);
+      } catch (err) {
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    dataFetch();
+  }, []);
+
+  if (isLoading) return <h2>Loading</h2>;
+
   return (
     <>
-      <h2>Shop</h2>
+      <Cards data={data} addToCart={addToCart} />
     </>
   );
 }
